9f04dbb746fa8b98aeea1f2935ee8ac84a87e8a5
Wouter Beets <beetwouter@gmail.com>
2022-02-24 09:16:28 +0100

    algo done

--- net.go
+++ net.go
@@ -1,16 +1,20 @@
 package net
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
 type synapse struct {
 	source      *neuron
 	destination *neuron
 	weight      float64
 	memory      *signal // when a signal passes through a synapse, it's id is stored in last signal to avoid loops
+	visited     bool
 }
 
 type neuron struct {
-	id   string
+	id   int
 	bias float64
 	out  []synapse // outgoing connections
 	in   []synapse // in connections
@@ -23,37 +27,53 @@ type signal struct {
 }
 
 func (n *neuron) eval(id int) signal {
+	fmt.Println("entering neuron", n.id)
 	//note that the signal passed here
 	for _, syn := range n.out {
-		syn.memory = &signal{id: id}
+		syn.visited = true
 	}
 
 	// get signal from input recursivly
 	var signals []signal
-	for _, syn := range n.in {
-		// we've hit an edge
-		if syn.memory != nil {
-			//check if its a loop or an input
-			if syn.memory.id == id {
-				sig := *syn.memory
-				syn.memory
-				return sig
-			} else { // its a memory
-				sig := *n.in[0].memory
-				return sig
+	for i := range n.in {
+		syn := n.in[i]
+		// we've hit a loop
+		if syn.visited {
+			if syn.memory == nil {
+				syn.memory = &signal{id: id}
 			}
+			sig := *syn.memory
+			fmt.Printf("mem %+v\n", sig)
+			return sig
 		}
+		// get input recursivly
+		fmt.Println("calling eval for", syn.source.id)
 		sig := syn.source.eval(id)
-		//manage memory here
+		fmt.Printf("got signal from %d, sig %+v\n", syn.source.id, sig)
+
+		// apply weight
 		sig.v *= syn.weight
+		fmt.Printf("sig after weigth %+v\n", sig)
+
+		// save signal
 		signals = append(signals, sig)
+
+		// undo visit
+		syn.visited = false
+
+		// leave memory if synapse ends in loop
+		if syn.memory != nil {
+			syn.memory = &sig
+		}
 	}
 
 	var sum float64
 	for _, sig := range signals {
 		sum += sig.v
 	}
-	return signal{v: sigmoid(sum * n.bias), id: id}
+	sig := signal{v: sigmoid(sum * n.bias), id: id}
+	fmt.Printf("returning sig %+v\n", sig)
+	return sig
 
 }
 
@@ -67,6 +87,7 @@ type net struct {
 func (n net) eval(input []float64) (output []float64) {
 	n.signalID += 1
 	for i, neuron := range n.in {
+		neuron.in[0].visited = true
 		neuron.in[0].memory = &signal{v: input[i], id: n.signalID}
 	}
 
@@ -80,6 +101,52 @@ func (n net) eval(input []float64) (output []float64) {
 	}
 
 	return output
+}
+
+func NewNet(in, out int) net {
+	var hidden int = 1
+
+	n := net{}
+	neurons := make([]neuron, in+hidden+out, in+hidden+out)
+	for i := range neurons {
+		neurons[i].id = i
+	}
+	n.in = neurons[0:in]
+	n.hidden = neurons[in : in+1]
+	n.out = neurons[in+1:]
+
+	// init input synapses
+	for i := range n.in {
+
+		// environment input
+		n.in[i].in = []synapse{{destination: &n.in[i]}}
+
+		// to hidden layer
+		n.in[i].out = []synapse{{
+			source:      &n.in[i],
+			destination: &n.hidden[0],
+		}}
+
+		// hidden layer input
+		n.hidden[0].in = append(n.hidden[0].in, n.in[i].out...)
+	}
+
+	// init output synapses
+	for i := range n.out {
+
+		// output to env
+		n.out[i].out = []synapse{{source: &n.out[i]}}
+
+		// hidden to output
+		n.out[i].in = []synapse{{
+			source:      &n.hidden[0],
+			destination: &n.out[i],
+		}}
+
+		// hidden layer output
+		n.hidden[0].out = append(n.hidden[0].out, n.out[i].in...)
+	}
+	return n
 }
 
 func sigmoid(x float64) float64 {
--- net_test.go
+++ net_test.go
@@ -1,48 +1,24 @@
 package net
 
 import (
-	"fmt"
 	"testing"
 )
 
-func TestCreateNet(t *testing.T) {
-	input := []*neuron{{id: "0"}, {id: "1"}}
-	output := []*neuron{{id: "2"}}
-	hidden := []*neuron{{id: "3"}, {id: "4"}}
-	synapses := []synapse{
-		{
-			source:      input[0],
-			destination: hidden[0],
-		},
-		{
-			source:      input[0],
-			destination: hidden[1],
-		},
-		{
-			source:      input[1],
-			destination: hidden[0],
-		},
-		{
-			source:      input[1],
-			destination: hidden[1],
-		},
-		{
-			source:      hidden[0],
-			destination: output[0],
-		},
-		{
-			source:      hidden[1],
-			destination: output[0],
-		},
+func TestNewNet(t *testing.T) {
+	inSize := 2
+	outSize := 2
+	net := NewNet(inSize, outSize)
+	if len(net.in) != inSize {
+		t.Error("len of in not", inSize)
 	}
-	input[0].out = synapses[0:2]
-	input[1].out = synapses[2:4]
-	hidden[0].in = synapses[0:2]
-	hidden[1].in = synapses[2:4]
-	hidden[0].out = synapses[4:5]
-	hidden[1].out = synapses[5:6]
-
-	for _, v := range synapses {
-		fmt.Println(v)
+	if len(net.out) != outSize {
+		t.Error("len of out not", inSize)
+	}
+	out := net.eval([]float64{1, 2})
+	if out[0] != 0.5 {
+		t.Error("net not returnign default value")
+	}
+	if out[1] != 0.5 {
+		t.Error("net not returnign default value")
 	}
 }
